7c93b81f2c746bf7db8d2d8b22cbfa40159a155b
niko neuhauser <niko.neuhauser@gmail.com>
2017-03-25 14:11:55 +0100

    faq

--- locales/en-US-faq.php
+++ locales/en-US-faq.php
@@ -2,16 +2,32 @@
 
 return [
 
-// 'Does Betterliving only operate in Kashmir?',
-// 'No, Betterliving is here for everybody in India. Everyone can be a
-// customer and also earn money with us.',
+// next
+'How do I pay for my Happiness Guide?',
+
+'<p>After registering, click the button that says “Buy and pay”. This way you are in the purchase process. You will have to make the payment through your PayPal account. In case you do not own a PayPal account, please register there. It is free and done in a few easy steps.</p>',
+
+// next
+'When is the first part of my Happiness Guide ready to download?',
+
+'<p>Right after we have received your payment, we will make sure you can download the first part of your Happiness Guide (usually within 24 hours). When you are logged in you can find the link in your personal download section.</p>',
+
+// next
+'When do I receive the other parts to complete my Happiness Guide?',
+
+'<p>After receiving the first part, you will be able to download the following ones every two weeks. There are eight parts in total. We will let you know when there is a new part ready to download via email.</p>',
 
 
 // next
-'How is the price for the Happiness Guide to pay?',
+'Where can I find my personal download section?',
 
-'<p>TODO</p>',
+'<p>After purchasing the Happiness Guide you will receive an email which includes your personal client identification number (ID). You will need this number to log into your account in order to get to the download section. This is where you can find the links to download the Happiness Guide parts.</p>',
 
+
+// next
+'I need help!',
+
+'<p>In case there is any problem you need help with, just contact us at info@betterliving.social anytime.</p>',
 
 
 ];
