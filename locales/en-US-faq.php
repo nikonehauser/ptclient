fb695bf9ac2a6112048e405e889e404ed669ad36
egal <niko.neuhauser@gmail.com>
2017-11-20 21:16:38 +0100

    update

--- locales/en-US-faq.php
+++ locales/en-US-faq.php
@@ -42,9 +42,9 @@ return [
 
 '<p>No! It\'s only a recommendation system for our customers. Such a kind of marketing systems works also worldwide in different industries.</p>
 
-<p>It really is enough if everybody only recruits three new customers in order to earn quite a big amount of money. Even if the average is less than three customers, the marketing system and commission flow will not stand still. You can watch the explanation video several times to fully understand how it works.</p>
+<p>It really is enough if everybody only recommends three new customers in order to earn quite a big amount of money. Even if the average is less than three customers, the marketing system and commission flow will not stand still. You can watch the explanation video several times to fully understand how it works.</p>
 
-<p>How to increase your commission are explained in the video in a realistic and understandable way. In case you recruit three customers you can still top your profit by recruiting new customers anytime you want!</p>
+<p>How to increase your commission are explained in the video in a realistic and understandable way. In case you recommend three customers you can still top your profit by recommending new customers anytime you want!</p>
 
 <p>You will earn much more money than you had paid for your Happiness Guide. Our Happiness Guide will show you a lot of ways to a happier life. Our marketing system offers a very easy and fast way to be happy.</p>
 
@@ -54,23 +54,23 @@ return [
 // next
 'How can I make money without constantly being active?',
 
-'<p>What makes this marketing system so unique is that you become a well-paid recommendation giver. As soon as you find customers, we will directly transfer your commissions to your privat bank account.</p>
+'<p>What makes this recommendation marketing system so unique is that you become a well-paid recommendation giver. As soon as you find customers, we will directly transfer your commissions to your privat bank account.</p>
 
-<p>Also you can get paid for customers that you  have recruited indirectly - without even knowing them. Of course it does not work by doing nothing. But all there is to do is recruit three customers.</p>
+<p>Also you can get paid for customers that you  have recommended indirectly - without even knowing them. Of course it does not work by doing nothing. But all there is to do is recommended three customers.</p>
 
-<p>From this point on you get access to the marketing system. New customers will be allocated to you, you don`t know these people and most possibly you won`t ever get to know them. For the first two customers you get '.\Tbmt\Localizer::fmtAdvertisedLvl1Amount().' each, from the third on you get '.\Tbmt\Localizer::fmtAdvertisedLvl2Amount().' each and a customer that has been allocated to you makes '.\Tbmt\Localizer::fmtAdvertisedIndirectAmount().' for you!</p>
+<p>From this point on you get access to the recommendation marketing system. New customers will be allocated to you, you don`t know these people and most possibly you won`t ever get to know them. For the first two customers you get '.\Tbmt\Localizer::fmtAdvertisedLvl1Amount().' each, from the third on you get '.\Tbmt\Localizer::fmtAdvertisedLvl2Amount().' each and a customer that has been allocated to you makes '.\Tbmt\Localizer::fmtAdvertisedIndirectAmount().' for you!</p>
 
-<p>Now this does not sound like a lot of money yet. But the marketing system´s mathematical algorithm lets you receive many million automatically. Just watch the video once again to make it clear.</p>',
+<p>Now this does not sound like a lot of money yet. But the recommendation marketing system´s mathematical algorithm lets you receive many million automatically. Just watch the video once again to make it clear.</p>',
 
 
 // next
 'Do I have to find all possible customers immediately?',
 
-'<p>You are the one who decides when and how many customers you recruit. If you are a customer of Betterliving you are authorized to recruit new customers and get paid for finding them. So it is your choice whether your payments roll in fast or slowly.</p>
+'<p>You are the one who decides when and how many customers you recommend. If you are a customer of Betterliving you are authorized to recommend new customers and get paid for finding them. So it is your choice whether your payments roll in fast or slowly.</p>
 
 <p>For example: If you joined Betterliving today and by doing so you became a customer of Betterliving. You paid the purchase price for the Happiness guide and got your customer number. The same week you´d find three new customers who all became customers of Betterliving as well and stated your customer number. That would be it.</p>
 
-<p>If every future customer acted the way you did, you would receive '.\Tbmt\Localizer::currencyFormatByCfg(48000).' two weeks later and after eight weeks it could be about ₹ 20,00,000 per week! You will receive all payments on your private bank account weekly. Of course you can get even more than this: Just recruit more customers. It is all in your hands - if you find more customers you will get higher commissions.</p>',
+<p>If every future customer acted the way you did, you would receive '.\Tbmt\Localizer::currencyFormatByCfg(48000).' two weeks later and after eight weeks it could be about ₹ 20,00,000 per week! You will receive all payments on your private bank account weekly. Of course you can get even more than this: Just recommend more customers. It is all in your hands - if you find more customers you will get higher commissions.</p>',
 
 
 // next
@@ -78,8 +78,13 @@ return [
 
 '<p>We balance your payments and transfer the money to your private bank account weekly. In conclusion: If you are successful, you will receive money every week.</p>
 
-<p>Please note: You will get paid only after we received your complete purchase price for your own Happiness Guide. It is not possible to offset your purchase price against the commissions you get for recruited customers.</p>',
-
+<p>Please note:
+<ul>
+  <li>You will get paid only after we received your complete purchase price for your own Happiness Guide</li>
+  <li>It is not possible to offset your purchase price against the commissions you get for recommended customers.</li>
+  <li>The bank needs for the transfer of your commission your pass photo and a photo of your PAN card. Please upload these two pictures on our website at the signup. Otherwise the bank cannot transfer your commission into your bank account</li>
+</ul>
+</p>',
 
 
 ];
